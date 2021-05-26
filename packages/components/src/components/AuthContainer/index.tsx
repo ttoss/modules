139774de4ee8c61b93fe9dab0cee479fa47eac8b
Rayza Oliveira <rayza.ocr@gmail.com>
2021-05-26 16:48:03 -0300

    feat: creating AuthContainer

---
+++ packages/components/src/components/AuthContainer/index.tsx
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import { Flex, Image, Link, Text } from 'theme-ui';
+
+import LogoDemo from '../../components/LogoDemo/index';
+
+type LinkButtonProps = {
+  children: React.ReactNode | React.ReactNodeArray;
+  href: string;
+  color?: string;
+};
+
+const LinkButton = ({ children, href, color }: LinkButtonProps) => {
+  return (
+    <Link
+      sx={{
+        marginTop: '24px',
+        textDecoration: 'underline',
+        cursor: 'pointer',
+        color,
+      }}
+      href={href}
+    >
+      {children}
+    </Link>
+  );
+};
+
+type AuthContainerProps = {
+  children: React.ReactNode | React.ReactNodeArray;
+  urlLogo?: string;
+  title: string;
+  links?: {
+    label: string;
+    href: string;
+    color?: string;
+  }[];
+};
+
+const AuthContainer = ({
+  children,
+  urlLogo,
+  title,
+  links,
+}: AuthContainerProps) => {
+  return (
+    <Flex
+      sx={{
+        maxWidth: '400px',
+        width: '100%',
+        border: 'solid 2px #444',
+        padding: '32px',
+        flexDirection: 'column',
+        alignItems: 'center',
+        color: 'text',
+        fontFamily: 'Arial',
+      }}
+    >
+      {urlLogo ? (
+        <Image sx={{ maxHeight: '120px', marginY: '14px' }} src={urlLogo} />
+      ) : (
+        <LogoDemo />
+      )}
+      <Text
+        sx={{
+          marginY: '24px',
+          textAlign: 'center',
+          width: '100%',
+          fontWeight: 'bold',
+          textTransform: 'uppercase',
+        }}
+      >
+        {title}
+      </Text>
+      {children}
+      {links?.map((link) => (
+        <LinkButton color={link.color} href={link.href}>
+          {link.label}
+        </LinkButton>
+      ))}
+    </Flex>
+  );
+};
+
+export default AuthContainer;
