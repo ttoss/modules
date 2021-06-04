f2b69373a4cc75f08fd69a391a85de89b1663d42
Pedro Arantes <arantespp@gmail.com>
2021-06-04 13:26:50 -0300

    fix: export modules

--- packages/modules/src/index.tsx
+++ packages/modules/src/index.tsx
@@ -1,3 +1,9 @@
-export * from './modules/auth';
+export { default as Amplify } from 'aws-amplify';
 
 export { default as ModulesProvider } from './providers/ModulesProvider';
+
+export { AuthProvider, Auth, useAuth } from './modules/auth';
+
+export { Form, yup, yupResolver, useForm } from './modules/forms';
+
+export { useNotifications } from './modules/notifications';
