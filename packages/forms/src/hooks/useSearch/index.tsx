74beb3540238fd3050854147990fd6739d06b223
Rayza Oliveira <rayza.ocr@gmail.com>
2021-07-01 20:06:36 -0300

    feat: adding onClickResult in the hook useSearch

--- packages/forms/src/hooks/useSearch/index.tsx
+++ packages/forms/src/hooks/useSearch/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
-import { OnSearch, Results } from 'src/models/search';
+import { OnSearch, Results, OnClickResult } from 'src/models/search';
 
-export const useSearch = ({ onSearch }: { onSearch: OnSearch }) => {
+export const useSearch = ({
+  onSearch,
+  onClickResult,
+}: {
+  onSearch: OnSearch;
+  onClickResult?: OnClickResult;
+}) => {
   const ref = React.useRef<HTMLInputElement>(null);
 
   const [results, setResults] = React.useState<Results>(null);
@@ -14,5 +20,5 @@ export const useSearch = ({ onSearch }: { onSearch: OnSearch }) => {
     }
   }, []);
 
-  return { onSearch, results, setResults, clear, ref };
+  return { onSearch, results, setResults, clear, ref, onClickResult };
 };
