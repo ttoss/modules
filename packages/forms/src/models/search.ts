f4b767d9cb678be09fed88e926d406378ed9c6ea
Rayza Oliveira <rayza.ocr@gmail.com>
2021-07-01 19:57:47 -0300

    feat: adding new type to OnClickResult

--- packages/forms/src/models/search.ts
+++ packages/forms/src/models/search.ts
@@ -3,3 +3,5 @@ export type Result = { id: string | number; value: string };
 export type Results = Result[] | null;
 
 export type OnSearch = (query: string) => void;
+
+export type OnClickResult = (idValue: string | number) => void;
