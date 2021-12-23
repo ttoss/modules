91494af21d5e3e92416c320bb22c2c06e7c72191
Pedro Arantes <arantespp@gmail.com>
2021-12-23 17:48:14 -0300

    feat: add cicd

---
+++ cicd/carlin.ts
@@ -0,0 +1,14 @@
+import { secrets } from './secrets';
+
+export default {
+  pipelines: ['pr', 'main', 'tag'],
+  slackWebhookUrl: secrets.slackWebhookUrl,
+  sshKey: './ssh-key',
+  sshUrl: 'git@github.com:ttoss/modules.git',
+  taskEnvironment: [
+    {
+      name: 'NPM_TOKEN',
+      value: secrets.npmToken,
+    },
+  ],
+};
