486601d96b3e91b4ac6c91d1ce0db4e0f10583c2
arthurlolico <arthur.lolico@gmail.com>
2022-02-03 18:49:40 -0300

    feat: correctly add cognito

---
+++ packages/auth/cloud/teste.spec.ts
@@ -0,0 +1,15 @@
+//import { template } from './cloudformation';
+
+/*type obj = {
+  getTemplate: (v: string) => string
+}
+
+const v: obj = {
+  getTemplate(v: instanceof template ){
+    return v;
+  }
+};*/
+
+//let temp = () => {return template};
+
+//console.log(temp);
