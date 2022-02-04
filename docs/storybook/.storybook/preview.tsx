6ef9cea3db705a80d39fd50aef75800065fafc28
Pedro Arantes <arantespp@gmail.com>
2022-02-04 12:32:30 -0300

    feat: add fonts to theme provider (#42)

--- docs/storybook/.storybook/preview.tsx
+++ docs/storybook/.storybook/preview.tsx
@@ -31,7 +31,12 @@ export const decorators = [
     const theme = themes[context.globals.theme];
 
     return (
-      <ThemeProvider theme={theme}>
+      <ThemeProvider
+        theme={theme}
+        fonts={[
+          'https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Overlock:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap',
+        ]}
+      >
         <NotificationsProvider>
           <Story />
         </NotificationsProvider>
