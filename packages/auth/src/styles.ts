1edcc65f76040f2e635a6c48f4bf5ffa51a0438f
Rayza <rayza.ocr@gmail.com>
2022-02-03 21:59:18 -0300

    feat: creating default style to input

---
+++ packages/auth/src/styles.ts
@@ -0,0 +1,11 @@
+export const defaultStyleInput = {
+  borderColor: 'muted',
+  borderWidth: 2,
+  fontFamily: 'body',
+  fontSize: 3,
+  paddingY: 4,
+  paddingX: 5,
+  '::placeholder': {
+    color: 'muted',
+  },
+};
