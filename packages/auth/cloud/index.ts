4d5045d8ae2448d0afa26141b83a5317b549fef0
unknown <arthur.lolico@gmail.com>
2022-02-08 17:21:18 -0300

    fix: corrected index.ts and other files

--- packages/auth/cloud/index.ts
+++ packages/auth/cloud/index.ts
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
 export { getTemplate } from './cloudformation';
-=======
-import { getTemplate } from "./cloudformation"
->>>>>>> parent of 4dc41aa (fix: correct index.ts)
-
-const template = getTemplate();
-
-export default template;
