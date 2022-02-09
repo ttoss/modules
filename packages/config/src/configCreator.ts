d5ec98d522e5e885a615b570076ed9408bd8206d
Pedro Arantes <arantespp@gmail.com>
2022-02-09 13:30:49 -0300

    fix: auth exports

--- packages/config/src/configCreator.ts
+++ packages/config/src/configCreator.ts
@@ -2,7 +2,7 @@ import deepmerge from 'deepmerge';
 
 const overwriteMerge = (_: any, sourceArray: any) => sourceArray;
 
-export const configCreator = <T extends Record<string, any>>(
-  defaultConfig: T = {} as T
-) => (config: T = {} as T) =>
-  deepmerge<T>(defaultConfig, config, { arrayMerge: overwriteMerge });
+export const configCreator =
+  <T extends Record<string, any>>(defaultConfig: T = {} as T) =>
+  (config: T = {} as T) =>
+    deepmerge<T>(defaultConfig, config, { arrayMerge: overwriteMerge });
