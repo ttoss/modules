63335263b300e0007aa96bdd137ecfedd5254f54
Rayza <rayza.ocr@gmail.com>
2022-02-14 14:34:27 -0300

    feat: separating triangulos theme from index.ts theme

--- docs/storybook/themes/index.ts
+++ docs/storybook/themes/index.ts
@@ -1,71 +1,9 @@
 import { theme as Siflor } from './siflor';
+import { theme as Triangulos } from './triangulos';
+
 import type { Theme } from '@ttoss/ui';
 
 export const themes: { [key: string]: Theme } = {
-  Triangulos: {
-    breakpoints: ['42em', '60em'],
-    space: [0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987],
-    fonts: {
-      body: 'Overlock',
-      heading: 'Josefin Sans',
-    },
-    fontSizes: [7, 10, 12, 17, 22, 29, 39, 51, 68, 90],
-    fontWeights: {
-      body: 400,
-      heading: 700,
-      bold: 900,
-    },
-    lineHeights: {
-      body: 1.5,
-      heading: 1.125,
-    },
-
-    forms: {
-      auth: {
-        input: {
-          borderColor: 'muted',
-          borderWidth: 2,
-          fontFamily: 'body',
-          fontSize: 3,
-          paddingY: 4,
-          paddingX: 5,
-          '::placeholder': {
-            color: 'muted',
-          },
-        },
-        link: {
-          fontFamily: 'body',
-          fontSize: 3,
-          color: 'text',
-          textDecoration: 'none',
-        },
-        containerLinks: {
-          justifyContent: 'space-between',
-          flexDirection: 'column',
-          gap: 6,
-          marginTop: 7,
-          color: 'text',
-        },
-        submitButtonContainer: {
-          justifyContent: 'space-between',
-          marginTop: 7,
-        },
-        submitButton: {
-          paddingX: 7,
-          paddingY: 6,
-          fontFamily: 'body',
-          fontSize: 3,
-        },
-        cardContainer: {
-          maxWidth: '340px',
-          fontFamily: 'body',
-          padding: 7,
-          paddingTop: 8,
-          border: 'none',
-          background: 'white',
-        },
-      },
-    },
-  },
   Siflor,
+  Triangulos,
 };
--- docs/storybook/themes/triangulos.ts
+++ docs/storybook/themes/triangulos.ts
@@ -17,11 +17,6 @@ export const theme: Theme = {
     body: 1.5,
     heading: 1.125,
   },
-  radii: {
-    none: 0,
-    partial: 6,
-    full: '100%',
-  },
   buttons: {
     primary: {
       paddingX: 7,
@@ -48,7 +43,6 @@ export const theme: Theme = {
       background: 'white',
     },
   },
-
   forms: {
     input: {
       borderColor: 'muted',
@@ -68,38 +62,6 @@ export const theme: Theme = {
       gap: 6,
       marginTop: 7,
       color: 'text',
-    },
-  },
-
-  borderWidths: [0, '1px'],
-  colors: {
-    transparent: '#ffffff00',
-    primary: '#127547',
-    secondary: '#457F8D',
-    text: '#393A3A',
-    background: '#F7F9F8',
-    accent: '#00C7FE',
-    highlight: '#0067D2',
-    muted: '#B9B9B9',
-    primaryVariant: '#008774',
-    secondaryVariant: '#7AB4C3',
-    alert: '#FF655B',
-    success: '#66AA00',
-    caution: '#FC6C00',
-    neutral: '#888888',
-    eucalipto: '#E79E49',
-    pinus: '#EBC36D',
-    cedroAustraliano: '#9F4F2A',
-    mognoAfricano: '#C18771',
-    teca: '#C19358',
-  },
-  shadows: ['0', '0px 4px 4px rgba(0, 0, 0, 0.15)'],
-  sizes: { container: '68em' },
-  zIndices: [0, 10, 100, 1000],
-  styles: {
-    root: {
-      fontFamily: 'body',
-      fontWeight: 'body',
     },
   },
 };
