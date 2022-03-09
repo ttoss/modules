c88afc0d2bf22dc0821a39262703b4fc3622e5be
Rayza <rayza.ocr@gmail.com>
2022-03-08 23:36:06 -0300

    docs: creating an example on storybook docs

--- docs/storybook/.storybook/preview.tsx
+++ docs/storybook/.storybook/preview.tsx
@@ -2,6 +2,11 @@ import { NotificationsProvider } from '@ttoss/notifications/src';
 import { ThemeProvider } from '@ttoss/ui';
 import AuthProvider from '@ttoss/auth/src/AuthProvider/AuthProvider';
 
+import { useLocale } from 'storybook-addon-locale';
+
+import enUS from '../translations/en-US.json';
+import ptBR from '../translations/pt-BR.json';
+
 import { themes } from '../themes';
 
 export const parameters = {
@@ -12,6 +17,8 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  defaultLocale: 'pt-BR',
+  locales: ['pt-BR', 'en-US'],
 };
 
 export const globalTypes = {
@@ -31,6 +38,15 @@ export const decorators = [
   (Story, context) => {
     const theme = themes[context.globals.theme];
 
+    const locale = useLocale();
+
+    const translations = {
+      'pt-BR': ptBR,
+      'en-US': enUS,
+    };
+
+    console.log('locale::', locale);
+
     return (
       <ThemeProvider
         theme={theme}
@@ -38,7 +54,7 @@ export const decorators = [
           'https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Overlock:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap',
         ]}
       >
-        <AuthProvider>
+        <AuthProvider translations={translations} initialLocale={locale}>
           <NotificationsProvider position="top-right">
             <Story />
           </NotificationsProvider>
