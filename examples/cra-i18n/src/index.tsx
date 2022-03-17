613f77b97fc6c40cf85c81a55a01eae4ded2c6a0
Pedro Arantes <arantespp@gmail.com>
2022-03-17 17:02:31 -0300

    fix: i18n import

--- examples/cra-i18n/src/index.tsx
+++ examples/cra-i18n/src/index.tsx
@@ -2,18 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-import { I18nProvider } from '@ttoss/i18n/src';
+import { I18nProvider } from '@ttoss/i18n';
 import { ThemeProvider } from '@ttoss/ui';
-
-// import ptBR from './lang/pt-BR.json';
-// import enUS from './lang/en-US.json';
-
-// const translations = {
-//   'pt-BR': ptBR,
-//   'en-US': enUS,
-// };
 
 ReactDOM.render(
   <React.StrictMode>
@@ -37,8 +28,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
